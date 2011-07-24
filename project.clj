e18088ef9f8985e58bc2fdc93f248dd833f12fb8
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-07-24 23:43:34 +0200

    - update project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
-(defproject de.karolski.sanity "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject de.karolski.sanity "0.1.0-SNAPSHOT"
+  :description "Transparent addition of types and assertions by
+  deduction on argument names to functions."
+  :author "Thomas Karolski"
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]])
